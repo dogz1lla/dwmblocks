4d1f1676bd1d9e2fc11b5bff8623753926e99b8c
dogz1lla <dogz1lla@t480arch.xyz>
2025-10-07 00:21:03 +0200

    new time format

--- blocks.def.h
+++ blocks.def.h
@@ -8,7 +8,7 @@ static const Block blocks[] = {
 	{"", "printf %s%s%s%s \"^c#DC7633^\" $($HOME/.local/bin/battery.sh) \"^d^\"",	60,		1},
 	{"", "printf %s%s%s%s \"^c#DC7633^\" \"\uf1c0 \" $(free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g) \"^d^\"",	30,		0},
 	{"", "printf %s%s%s%s \"^c#FFD900^\" \"\uf073 \" $(date '+%b %d (%a)') \"^d^\"",		5,		0},
-	{"", "printf %s%s%s%s \"^c#FFFC72^\" \"\uf017 \" $(date '+%I:%M%p') \"^d^ \"",		    5,		0},
+	{"", "printf %s%s%s%s \"^c#FFFC72^\" \"\uf017 \" $(date '+%H:%M') \"^d^ \"",		    5,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
